c48c337fba1571760ee4ac9f88b867869a87cb37
Daniel Stansberry <dstansberry@widercircle.com>
2024-02-12 22:33:20 -0800

    Update demo studio.

--- demo/panda.config.ts
+++ demo/panda.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "@pandacss/dev";
-import { createKyotoPreset } from "./packages/themes/kyoto";
+import { createKyotoPreset } from "../packages/themes/kyoto";
 
 export default defineConfig({
   // Whether to use css reset
---
+++ demo/public/studio/_astro/token-group.kTnyHrk0.js
@@ -0,0 +1,1 @@
+import{p as a}from"./factory.P08CoaBK.js";const p=a("div",{base:{display:"flex",flexDir:"column",gap:"3",width:"full",marginTop:"5"}});export{p as T};
