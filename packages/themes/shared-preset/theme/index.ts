84e633fd15a6e2c61d649ec2c6d31d3b465a0906
Daniel Stansberry <danielsstansberry@gmail.com>
2024-02-16 00:53:51 -0800

    Add slot recipes.

--- packages/themes/shared-preset/theme/index.ts
+++ packages/themes/shared-preset/theme/index.ts
@@ -1,24 +1,25 @@
-import type { PresetOptions } from '../../types'
-import createSharedBreakpoints from './breakpoints'
-import type { BreakpointsPresetOptions } from './breakpoints/types'
-import keyframes from './keyframes'
-import { recipes } from './recipes'
-import createSharedSemanticTokens from './semantic-tokens'
-import textStyles from './text-styles'
-import createSharedTokens from './tokens'
+import type { PresetOptions } from "../../types";
+import createSharedBreakpoints from "./breakpoints";
+import type { BreakpointsPresetOptions } from "./breakpoints/types";
+import keyframes from "./keyframes";
+import { recipes, slotRecipes } from "./recipes";
+import createSharedSemanticTokens from "./semantic-tokens";
+import textStyles from "./text-styles";
+import createSharedTokens from "./tokens";
 
 type SharedThemeType = {
-  breakpointOptions?: BreakpointsPresetOptions
-  tokenOptions?: PresetOptions
-}
+  breakpointOptions?: BreakpointsPresetOptions;
+  tokenOptions?: PresetOptions;
+};
 
 const sharedTheme = (options?: SharedThemeType) => ({
   semanticTokens: createSharedSemanticTokens(),
   breakpoints: createSharedBreakpoints(options?.breakpointOptions),
   keyframes,
   recipes,
+  slotRecipes,
   textStyles,
   tokens: createSharedTokens(options?.tokenOptions ?? {}),
-})
+});
 
-export default sharedTheme
+export default sharedTheme;
